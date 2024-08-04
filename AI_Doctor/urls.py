e28c87b2d8fc7cb9c3c6d51821283753385a4960
williyam-m <williyam64@gmail.com>
2024-08-04 20:43:45 +0530

    create smart_symptom_checker app

--- AI_Doctor/urls.py
+++ AI_Doctor/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('chat.urls')),
+    path('', include('smart_symptom_checker.urls')),
 ]
---
+++ smart_symptom_checker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartSymptomCheckerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smart_symptom_checker'
